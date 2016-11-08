fab3431ec5310026d1a202ca441e6c9634d331a9
Alexandre Andrieux <alex@icosacid.com>
2016-11-08 07:39:58 +0100

    JSON data structure with some injectable content

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Scripts for project Infinite
+ *
+ * @since 2016-11-08
+ * @author Alexandre Andrieux <alex@icosacid.com>
+ */
+
+/** Script executed on start **/
+jQuery(document).ready(function() {
+    
+});
