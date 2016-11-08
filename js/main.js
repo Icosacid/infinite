093fd6a2554e95e490ad663de139d5590a342635
Alexandre Andrieux <alex@icosacid.com>
2016-11-08 10:10:20 +0100

    Migrate hexagon HTML block to Underscore template

--- js/main.js
+++ js/main.js
@@ -15,12 +15,22 @@ jQuery(document).ready(function() {
 
     // Load JSON content
     jQuery.getJSON("data.json", function(data) {
-        // Retrieve elements
+        // Retrieve some elements (examples)
         var bannerBkgPic = data.banner.background_pic_url;
         var splitBkgPic = data.split.background_pic_url;
+        var featuredIntroText = data.featured.news_intro_text;
 
-        // Inject in DOM without parallax (example)
+        // Inject text in DOM (example)
+        jQuery('.featured .unit p.intro').html(featuredIntroText);
+
+        // Inject images in DOM without parallax (example)
         //jQuery('.banner .mainpic').css('background-image', 'url(' + bannerBkgPic + ')');
+
+        // Inject views with Underscore templates
+        _([1, 3, 7, 14, 666, 9, 8, 12, 50]).each(function(e) {
+            var view = Infinite.views.hexagon('Artwork title', 'Awesome description ' + e);
+            jQuery('.hexblock').append(view);
+        });
 
         // Parallax effects
         jQuery('.mainpic').parallax({imageSrc: bannerBkgPic});
@@ -71,4 +81,34 @@ Infinite.buttonListeners = function() {
             Infinite.loadFeedback.off();
         }, 1000);
     });
+};
+
+// Shape DOM with Underscore templates
+Infinite.views = {};
+/**
+ * Hexagon view constructor
+ *
+ * @param {string} title Artwork title
+ * @param {string} description Artwork description
+ *
+ * @returns {string} HTML view for an hexagon
+ */
+Infinite.views.hexagon = function(title, description) {
+
+    var template = _.template('' +
+        '<div class="hexagon">' +
+        '  <div class="hex1">' +
+        '    <div class="hex2">' +
+        '      <div class="content click-loader">' +
+        '        <div class="inside">' +
+        '          <p><%= title %></p>' +
+        '          <hr/>' +
+        '          <p><%= description %></p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>');
+    
+    return template({title: title, description: description});
 };
